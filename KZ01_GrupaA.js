ad52840fc64a34db1219a4a4717488fca72aca42
Antonio Pavlović <anpavlovic@student.unipu.hr>
2023-03-29 19:10:35 +0200

    Drugi komit

--- KZ01_GrupaA.js
+++ KZ01_GrupaA.js
@@ -1,16 +1,71 @@
 //1 (3 BODA)
 //Napisi funkciju koja ispisuje parne brojeve od 0 do 100 (ukljucujuci 100)
+function parni() {
+    for (let i = 0; i <= 100; i += 2) {
+        console.log(i)
+    }
+}
+parni();
 
 //2 (3 BODA)
 //Napisi funkciju koja koristi switch selekciju za pretvaranje numerickog unosa u dan u tjednu. Za neispravan unos ispisi prikladnu poruku.
 //INPUT: 1; OUTPUT: "Ponedjeljak";
 //INPUT: 5; OUTPUT: "Petak"; 
+function dani(input) {
+    switch (input) {
+        case 1: {
+            console.log("Ponedjeljak");
+            break;
+        }
+        case 2: {
+            console.log("Utorak");
+            break;
+        }
+        case 3: {
+            console.log("Srijeda");
+            break;
+        }
+        case 4: {
+            console.log("Četvrtak");
+            break;
+        }
+        case 5: {
+            console.log("Petak");
+            break;
+        }
+        case 6: {
+            console.log("Subota");
+            break;
+        }
+        case 7: {
+            console.log("Nedjelja");
+            break;
+        }
+        default: {
+            console.log("Unos broja mora biti u rasponu od 1 do 7 !");
+            break;
+        }
+    }
+}
+//Poziv funkcije 
+dani(5);
+dani(8);
 
 //3 (4 BODA)
 //Napisi funkciju koja okrece uneseni string i broji charactere ne uzimajuci u obzir prazna mjesta (eng. whitespaces)
 //INPUT: "Pisanje zadataka nije jednostavan posao";
 //OUTPUT: ("oasop navatsondej ejin akatadaz ejnasiP", 35)
 
+function reverse(param) {
+    let a = []
+    let i = 0;
+    for (let j = param.length - 1; j <= param.length; j--) {
+        a.push[j];
+    }
+    console.log(a)
+}
+
+reverse("Pero");
 //4 (4 BODA)
 //Napisi funkciju koja vraca najkraci kljuc u danom objektu
 const somePeople = {
